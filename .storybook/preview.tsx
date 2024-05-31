9c8abf9966ae4701835b5cc7a2e96f97aa49dace
Gustavo Henrique <ghguga123@gmail.com>
2024-05-30 23:50:56 -0300

    chore: add config to Storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,33 @@
+import type { Preview } from "@storybook/react";
+import React from "react";
+import "../src/app/globals.css";
+import { baiJamjuree, roboto } from "../src/app/layout";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: "default",
+      values: [
+        { name: "stone", value: "#027435" },
+        { name: "default", value: "#eaeaea" },
+      ],
+    },
+  },
+  decorators: [
+    (Story) => (
+      <main
+        className={`${roboto.variable} ${baiJamjuree.variable} font-sans relative`}
+      >
+        <Story />
+      </main>
+    ),
+  ],
+};
+
+export default preview;
